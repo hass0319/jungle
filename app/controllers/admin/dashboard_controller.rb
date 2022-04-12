e7bb3d29df04bf9a67c3c8842cab1b4291cf38b2
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-04-12 10:56:58 +0000

    adds admin dashboard (category and products count))

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'].to_s, password: ENV['PASSWORD'].to_s
+
   def show
+    @categories_count = Category.count(:all)
+    @products_count   = Product.count(:all)
   end
 end
