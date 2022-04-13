d86264f8c99f8c8293785f2eb115a28fcee4f882
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-04-13 09:17:06 +0000

    adds user login and signup page. NOTE:login and logout doesnot work

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
 
   get '/about' => 'about#index'
+  # resources :about, only: [:index]
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+
 
   resource :cart, only: [:show] do
     post   :add_item
